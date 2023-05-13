10393180b8dcdbafe65f613665309a4e9d968917
2gblue <107160038+2gblue@users.noreply.github.com>
2023-05-13 17:51:54 +0800

    index improvement, removed scss fdr

--- indexfunctions.js
+++ indexfunctions.js
@@ -1,3 +1,26 @@
 function handleLinkClick(url) {
   window.location.href = url;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  var animatedText = document.getElementById("animatedText");
+  var textContent = animatedText.textContent;
+  animatedText.textContent = "";
+
+  var currentIndex = 0;
+
+  function animateText() {
+    if (currentIndex >= textContent.length) {
+      currentIndex = 0;
+    }
+
+    animatedText.textContent += textContent[currentIndex];
+    currentIndex++;
+
+    if (currentIndex <= textContent.length) {
+      setTimeout(animateText, 100);
+    }
+  }
+
+  animateText();
+});
